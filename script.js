6e287f08ac282a2052da14890db36a2706d34ba7
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-11-13 17:34:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+  if(arr.length<2)
+  {
+	  return 0;
+  }
+	const totalCost=arr.reduce((accumulator,currentValue)=>{
+		arr.sort((a,b)=>a-b);
+
+		const combinedCost=arr[0]+arr[1];
+		arr.splice(0,2);
+		return accumulator+combinedCost;
+	},0);
+	return totalCost;
 }
 
 module.exports=mincost;
